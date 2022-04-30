df8593de3824257890cdeecd26a75680d11ab278
Kate Johnson <johnson.kat.g@gmail.com>
2022-04-30 01:53:45 -0400

    added react app front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="auth-btn">
+        <button>Authenticate</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <MoralisProvider serverUrl={process.env.REACT_APP_SERVER_URL} appId={process.env.REACT_APP_APPID}>
+      <App />
+    </MoralisProvider>
+  </React.StrictMode>
+);
